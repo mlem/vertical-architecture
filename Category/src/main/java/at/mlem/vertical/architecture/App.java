61f041c1cb5ac52cdae425e73b27bd297452cc48
Martin Lemanski <martin.lemanski@gmx.at>
2017-05-13 20:40:07 +0200

    initial setup of 3 verticals

---
+++ Category/src/main/java/at/mlem/vertical/architecture/App.java
@@ -0,0 +1,13 @@
+package at.mlem.vertical.architecture;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
